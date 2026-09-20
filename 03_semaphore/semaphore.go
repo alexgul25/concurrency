d98802555af67b327fc9830dc24d850f1df05896
alexgul25 <fine.9ulyaev@yandex.ru>
2026-09-21 00:44:04 +0300

    feature: implement semaphore

--- 03_semaphore/semaphore.go
+++ 03_semaphore/semaphore.go
@@ -1,25 +1,38 @@
 package semaphore
 
+import (
+	"primitives/internal/futex"
+	"sync/atomic"
+)
+
 type Semaphore struct {
 	permits uint32
 }
 
 func New(n int) *Semaphore {
-	panic("не реализовано")
+	return &Semaphore{permits: uint32(n)}
 }
 
 func (s *Semaphore) Acquire() {
-	panic("не реализовано")
+	for {
+		curPermits := atomic.LoadUint32(&s.permits)
+		if curPermits != 0 && atomic.CompareAndSwapUint32(&s.permits, curPermits, curPermits-1) {
+			return
+		}
+		futex.Wait(&s.permits, 0)
+	}
 }
 
 func (s *Semaphore) TryAcquire() bool {
-	panic("не реализовано")
+	curPermits := atomic.LoadUint32(&s.permits)
+	return curPermits != 0 && atomic.CompareAndSwapUint32(&s.permits, curPermits, curPermits-1)
 }
 
 func (s *Semaphore) Release() {
-	panic("не реализовано")
+	atomic.AddUint32(&s.permits, 1)
+	futex.Wake(&s.permits)
 }
 
 func (s *Semaphore) Available() int {
-	panic("не реализовано")
+	return int(atomic.LoadUint32(&s.permits))
 }
