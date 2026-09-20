96de8e42c9366208c5a07197e13f43644e4d9b13
alexgul25 <fine.9ulyaev@yandex.ru>
2026-09-21 02:56:59 +0300

    feature: implement waitgroup

--- 04_waitgroup/waitgroup.go
+++ 04_waitgroup/waitgroup.go
@@ -1,17 +1,36 @@
 package waitgroup
 
+import (
+	"primitives/internal/futex"
+	"sync/atomic"
+)
+
 type WaitGroup struct {
 	count uint32
 }
 
 func (wg *WaitGroup) Add(delta int) {
-	panic("не реализовано")
+	atomic.AddUint32(&wg.count, uint32(delta))
 }
 
 func (wg *WaitGroup) Done() {
-	panic("не реализовано")
+	newCount := atomic.AddUint32(&wg.count, ^uint32(0))
+
+	if newCount == ^uint32(0) {
+		panic("count has overflowed or gone into the negative!!!")
+	}
+
+	if newCount == 0 {
+		futex.WakeAll(&wg.count)
+	}
 }
 
 func (wg *WaitGroup) Wait() {
-	panic("не реализовано")
+	for {
+		curCount := atomic.LoadUint32(&wg.count)
+		if curCount == 0 {
+			return
+		}
+		futex.Wait(&wg.count, curCount)
+	}
 }
