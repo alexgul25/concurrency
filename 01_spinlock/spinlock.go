55973455281ec4177a75c580fc81f79fdfd24962
alexgul25 <fine.9ulyaev@yandex.ru>
2026-09-20 03:15:07 +0300

    feature: implement spinlock

--- 01_spinlock/spinlock.go
+++ 01_spinlock/spinlock.go
@@ -7,15 +7,19 @@ type Spinlock struct {
 }
 
 func (s *Spinlock) Lock() {
-	panic("не реализовано")
+	for !s.locked.CompareAndSwap(false, true) {
+	}
 }
 
 func (s *Spinlock) TryLock() bool {
-	panic("не реализовано")
+	return s.locked.CompareAndSwap(false, true)
 }
 
 func (s *Spinlock) Unlock() {
-	panic("не реализовано")
+	if s.locked.CompareAndSwap(true, false) {
+		return
+	}
+	panic("Unlock without Lock!!!")
 }
 
 type TTAS struct {
@@ -23,13 +27,20 @@ type TTAS struct {
 }
 
 func (s *TTAS) Lock() {
-	panic("не реализовано")
+	for s.locked.Load() {
+	}
+
+	for !s.locked.CompareAndSwap(false, true) {
+	}
 }
 
 func (s *TTAS) TryLock() bool {
-	panic("не реализовано")
+	return !s.locked.Load() && s.locked.CompareAndSwap(false, true)
 }
 
 func (s *TTAS) Unlock() {
-	panic("не реализовано")
+	if s.locked.CompareAndSwap(true, false) {
+		return
+	}
+	panic("Unlock without Lock!!!")
 }
